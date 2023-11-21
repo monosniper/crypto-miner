73a014ee2ac1be3afc80550ff1df8c71efbfc05b
Andrey <teryaev.2004@mail.ru>
2023-11-21 17:49:23 +0300

    Add loading and empty text for servers plans

--- src/components/Servers/ServersPlans/ServersPlans.tsx
+++ src/components/Servers/ServersPlans/ServersPlans.tsx
@@ -1,22 +1,52 @@
 import { ServersPlansItem } from "./ServersPlansItem/ServersPlansItem";
 import styles from "./ServersPlans.module.css";
 import { useGetAllServersQuery } from "@/redux/api/serversApi";
+import { useLoading } from "@/hooks";
+import { useState } from "react";
+import { CoinSkelet, EmptyText } from "@/components";
 
 export const ServersPlans = () => {
-  const { data: serversList } = useGetAllServersQuery(null);
+  const {
+    data: serversList,
+    isLoading: serversIsLoading,
+    isFetching: serversIsFetching,
+  } = useGetAllServersQuery(null);
+  const [skeletItems] = useState(Array(6).fill(0));
+
+  const loading = useLoading(serversIsLoading, serversIsFetching);
 
   return (
     <div className="flex flex-wrap -m-2">
-      {serversList?.map((el) => {
-        return (
-          <div className="w-full md:w-1/2 lg:w-1/3 p-2">
-            <ServersPlansItem
-              className={el.isHot ? styles.beneficial : "h-full"}
-              data={el}
-            />
-          </div>
-        );
-      })}
+      {!loading ? (
+        <>
+          {serversList && serversList.length > 0 ? (
+            serversList.map((el) => {
+              return (
+                <div className="w-full md:w-1/2 lg:w-1/3 p-2">
+                  <ServersPlansItem
+                    className={el.isHot ? styles.beneficial : "h-full"}
+                    data={el}
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <div className="flex flex-col flex-grow">
+              <EmptyText text="Нет получилось получить данные" />
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          {skeletItems.map((_, idx) => {
+            return (
+              <div key={idx} className="w-full sm:w-1/2 lg:w-1/3 p-2">
+                <CoinSkelet />
+              </div>
+            );
+          })}
+        </>
+      )}
     </div>
   );
 };
