e3a5551e9da10a82dcd1be573ca0eced1712bc6c
Andrey <teryaev.2004@mail.ru>
2023-12-05 17:24:57 +0300

    use socket_url from env

--- src/core/socket.ts
+++ src/core/socket.ts
@@ -2,6 +2,8 @@ import Cookies from "js-cookie";
 
 const token = Cookies.get("token");
 
-const socket = new WebSocket("ws://188.116.20.163:1337?token=" + token);
+const socket = new WebSocket(
+  import.meta.env.VITE_SOCKET_URL + "?token=" + token
+);
 
 export default socket;
