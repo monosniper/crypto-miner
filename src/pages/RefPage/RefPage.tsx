de742683b68cb85e4ca2d04ce3c0e3eb6471d8b8
Andrey <teryaev.2004@mail.ru>
2024-01-03 23:53:56 +0300

    ix

--- src/pages/RefPage/RefPage.tsx
+++ src/pages/RefPage/RefPage.tsx
@@ -1,4 +1,4 @@
-import { Title } from "@/components";
+import { Attention, Title } from "@/components";
 import { CopyIcon } from "@/components/icons/CopyIcon";
 import { FieldWrapper, TextField } from "@/components/ui";
 import { user } from "@/redux/slices/userSlice";
@@ -17,19 +17,13 @@ export const RefPage = () => {
         <Title className="flex lg:hidden pb-6" title={t("referral system")} />
       </div>
 
-      <div className="bg-base-200 border border-base-border-100 rounded-xl p-6">
-        <div className="flex flex-wrap -m-5">
-          <div className="w-full2 p-5">
-            <div className="flex flex-col gap-6">
-              <p className="text-base">
-                Пригласите друзей и получайте 10% от всех их пополнений.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Attention
+        className="p-6"
+        title={t("pay-attention")}
+        content={<AttentionContent />}
+      />
 
-      <div className="box p-6 mt-8">
+      <div className="box p-6 mt-16">
         <FieldWrapper title={t("your referral link")}>
           <TextField
             className="[&>input]:!text-gray-1"
@@ -46,7 +40,7 @@ export const RefPage = () => {
                   if (!userData) return;
 
                   copyText(
-                    `https://www.hogyx.io?ref_code=${userData.ref_code}`,
+                    `https://www.hogyx.io?ref_code=${userData.ref_code}`
                   );
 
                   toast.success(t("the text has been copied"));
@@ -67,16 +61,16 @@ export const RefPage = () => {
 
               <span>{userData?.total_refs || 0}</span>
             </div>
-          </div>
-        </div>
-        <div className="box p-6 w-full sm:flex-1">
-          <div className="flex flex-col gap-4">
+
             <div className="flex justify-between items-center">
               <b>{t("the number of deposits")}:</b>
 
               <span>{userData?.total_refs_amount || 0}</span>
             </div>
-
+          </div>
+        </div>
+        <div className="box p-6 w-full sm:flex-1">
+          <div className="flex flex-col gap-4">
             <div className="flex justify-between items-center">
               <b>{t("the percentage received")}:</b>
 
@@ -86,5 +80,17 @@ export const RefPage = () => {
         </div>
       </div>
     </div>
+  );
+};
+
+const AttentionContent = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <div>
+        <p>{t("invite your friends and get 10% of all their deposits")}</p>
+      </div>
+    </>
   );
 };
